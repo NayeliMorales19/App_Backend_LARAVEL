b5a5326a5ae9d882f4aeb0d8734325ab87416d48
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-05-14 15:54:59 -0600

    modificacion de ruta v2

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumnoController;
 
 Route::get('/', function () {
-    return view('/login');
-
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
